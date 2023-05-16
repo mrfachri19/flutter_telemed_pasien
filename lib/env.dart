3c8581c992bccdf19e5596a83021bf6882f25217
mrfachri19 <fachryfachry1997@gmail.com>
2023-05-16 15:04:49 +0700

    env

---
+++ lib/env.dart
@@ -0,0 +1,6 @@
+abstract class Env {
+  // dev
+  static const baseUrl = 'https://dev.rsud-kita.pkp.co.id/v2/';
+  static const defaultMessage = 'Pesan tidak dikenal';
+  static const defaultErrorMessageTitle = 'Terjadi Kesalahan';
+}
